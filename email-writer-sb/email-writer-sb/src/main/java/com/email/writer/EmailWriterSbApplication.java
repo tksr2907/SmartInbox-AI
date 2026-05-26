e5def555889688827a9b3e9d4d437bacc74d5aaa
tksr2907 <tks292002@gmail.com>
2026-05-27 01:52:56 +0530

    Finished implementation of AI email assistant

--- email-writer-sb/email-writer-sb/src/main/java/com/email/writer/EmailWriterSbApplication.java
+++ email-writer-sb/email-writer-sb/src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailWriterSbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailWriterSbApplication.class, args);
-	}
+        SpringApplication.run(EmailWriterSbApplication.class, args);
+    }
 
 }
